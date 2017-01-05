585d89c4ef5fac75459f282f0f7b9dc113235cd9
Igor S <Igor_Somik@web.de>
2017-01-05 19:28:57 +0100

    Neues Framework engefügt und das Spiel umdesignt
    Ein neues Framework wurde in das Spiel eingefügt. Des Weiteren wurde das
    Spielbrett umgestaltet damit es an das neue Framework angepasst ist.
    Einige Features des neue Frameworks wie Pathfinder Algorithmus,
    Highlighten der Pfade, grundlegendes Balancing, simple Animation bei
    Bewegung, Angriff auf andere Einheiten und simples UI wurden eingefügt.

---
+++ Game_Engineering_Project/Assets/Created Input/Scripts/TestCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCollider : MonoBehaviour {
+
+    
+
+    void OnTriggerEnter (Collider collidedObject)
+    {
+        //gameController.playerInAttackRange(collidedObject.transform.parent.name);
+        Debug.Log(collidedObject.transform.position);
+    }
+
+    void OnTriggerExit (Collider collidedObject)
+    {
+        //gameController.playerInAttackRange("no Player in range");
+        //Debug.Log(collidedObject.name + "Player TWOOOO EXIIIT");
+    }
+}
---
+++ Game_Engineering_Project/Assets/TBS Framework/Scripts/Tutorial/GUIController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIController : MonoBehaviour
+{
+    public CellGrid CellGrid;
+    public GameObject ChangeTurnScreen;
+    public GameObject EndScreen;
+    public Text EndScreenText;
+
+    void Start()
+    {
+        CellGrid.TurnEnded += OnTurnEnded;
+        CellGrid.GameEnded += OnGameEnded;
+    }
+
+    public void quitAllpication()
+    {
+        Application.Quit();
+    }
+
+    public void finishTurn()
+    {
+        CellGrid.EndTurn();
+    }
+
+
+    private void ShowOrHideNextTurnMessege()
+    {
+        ChangeTurnScreen.SetActive(false);
+    }
+
+
+    private void OnTurnEnded(object sender, EventArgs e)
+    {
+        if((sender as CellGrid).CurrentPlayer is HumanPlayer)
+        {
+            ChangeTurnScreen.SetActive(true);
+
+            Invoke("ShowOrHideNextTurnMessege", 1);
+        }
+    }
+
+
+    private void OnGameEnded(object sender, EventArgs e)
+    {
+        if((sender as CellGrid).CurrentPlayerNumber == 0)
+        {
+            EndScreenText.text = "Glückwunsch Sie haben gewonnen!";
+        }
+        else
+        {
+            EndScreenText.text = "Leider haben Sie verloren :(";
+        }
+        EndScreen.SetActive(true);
+    }
+
+
+}
